8ca08d1f267a3e1913d2beb6c8e87e67f63a61bc
HARI-KIRAN-REDDY <109365998+HARI-KIRAN-REDDY@users.noreply.github.com>
2025-08-03 09:40:03 +0530

    test in progress

--- daily_bulk_deals_update_job.py
+++ daily_bulk_deals_update_job.py
@@ -9,22 +9,22 @@ def save_bulk_csv_file():
     # Set headers to mimic browser
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-        "Accept": "text/csv",
+        "Content-Type": "application/json; charset=utf-8",
+        "Accept": "*/*",
         "Referer": "https://www.nseindia.com/market-data/large-deals",
         "Origin": "https://www.nseindia.com",
-        "X-Requested-With": "XMLHttpRequest",
     }
 
     response = requests.get(url, headers=headers, timeout=10)
 
-    if response.status_code == 200:
-        filename = f"bulk.csv"
-        with open(filename, "w", newline="", encoding="utf-8") as f:
-            f.write(response.text)
-        print(f"✅ Saved Bulk Deals to `{filename}`")
-    else:
-        print(f"❌ Request failed: {response.status_code}")
-        print("Response:", response.text)
+    # if response.status_code == 200:
+    #     filename = f"bulk.csv"
+    #     with open(filename, "w", newline="", encoding="utf-8") as f:
+    #         f.write(response.text)
+    #     print(f"✅ Saved Bulk Deals to `{filename}`")
+    # else:
+    #     print(f"❌ Request failed: {response.status_code}")
+    #     print("Response:", response.text)
 
 def prepare_data():
     df = pd.read_csv("bulk.csv")
@@ -44,6 +44,7 @@ def prepare_data():
 
 
 save_bulk_csv_file()
+
 
 
 
