28a8e9d60cc118db84ed006458b9955b35e85bd2
HARI-KIRAN-REDDY <109365998+HARI-KIRAN-REDDY@users.noreply.github.com>
2025-12-20 17:26:19 +0530

    Update historical_bulk_deal_modification_job.py

--- historical_bulk_deal_modification_job.py
+++ historical_bulk_deal_modification_job.py
@@ -12,20 +12,21 @@ response = requests.get(URL,  timeout=10)
 response.raise_for_status()
 df = pd.read_csv(StringIO(response.text))
 
-# signed value (BUY +, SELL -)
-signed = (
-    df["Quantity Traded"]
-    * df["Trade Price / Wght. Avg. Price"]
-    * np.where(df["Buy/Sell"] == "BUY", 1, -1)
-)
+
+df['Quantity Traded'] = np.where(df['Buy/Sell'] == 'BUY',
+                                 df['Quantity Traded'],
+                                 -df['Quantity Traded'])
+df['net_value'] = np.where(df['Buy/Sell'] == 'BUY',
+                           df['Trade Price / Wght. Avg. Price']*df["Quantity Traded"],
+                           -df['Trade Price / Wght. Avg. Price']*df["Quantity Traded"])
 
 # NET_VALUE per Date + Symbol + Client Name
-df["NET_VALUE"] = (
-    signed.groupby([df["Date"], df["Symbol"], df["Client Name"]])
-    .transform("sum")
-)
+df = df.groupby(["Date", "Symbol", "Client Name"])['net_value'].sum().reset_index()
 
 # filter by threshold
-df = df[df["NET_VALUE"].abs() >= THRESHOLD]
+df = df[df["net_value"].abs() >= THRESHOLD]
+df['action'] = np.where(df["net_value"] > 0, 'BUY', 'SELL')
+
 
 df.to_csv('modified_historical_bulk_deals.csv', index=False)
+
